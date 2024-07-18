d870a9049e0332e86f374faf6a9aa119e917d17c
parththakark23 <parththakark23@gmail.com>
2024-07-18 23:05:04 +0530

    feat: added setup for react query

--- app/(tabs)/schedule.tsx
+++ app/(tabs)/schedule.tsx
@@ -1,5 +1,12 @@
 import { ThemedText } from "@/components/ThemedText";
 import { StyleSheet, Text } from "react-native";
+import ReactDOM from "react-dom/client";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const styles = StyleSheet.create({
   headerImage: {
@@ -17,7 +24,32 @@ const styles = StyleSheet.create({
   },
 });
 
+const queryClient = new QueryClient();
+
 const ScheduleScreen = () => {
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools />
+        <ComponentData />
+      </QueryClientProvider>
+    </>
+  );
+};
+
+const ComponentData = () => {
+  // const { isPending, error, data, isFetching } = useQuery({
+  //   queryKey: ["repoData"],
+  //   queryFn: async () => {
+  //     const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  //     return await response.json();
+  //   },
+  // });
+
+  // if (isPending) return "Loading...";
+
+  // if (error) return "An error has occurred: " + error.message;
+
   return (
     <>
       <ThemedText>My schedule will look like this</ThemedText>
